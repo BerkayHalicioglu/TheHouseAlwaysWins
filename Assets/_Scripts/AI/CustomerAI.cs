b4322379457c509ef3c0a41f9227a8b9b1cdee46
keremozt <keremoztopuzzz@gmail.com>
2026-05-10 17:47:33 +0300

    feat: send customers to reserved blackjack seats

--- Assets/_Scripts/AI/CustomerAI.cs
+++ Assets/_Scripts/AI/CustomerAI.cs
@@ -7,11 +7,16 @@ public class CustomerAI : MonoBehaviour
     [Header("Cheating")] // bold font 
     [SerializeField, Range(0f, 1f)] private float cheatChance = 0.25f; // npc cheating probability
 
+    [Header("Movement")]
+    [SerializeField] private float destinationReachedDistance = 0.5f;
+
     public CustomerState CurrentState { get; private set; } // current behaviour
     public bool IsCheater { get; private set; } // cheat flag
     public bool CanBeInteractedWith { get; private set; } = true; // npc interaction flag
 
     private NavMeshAgent agent; // navmesh 
+    private BlackjackTable currentTable;
+    private Transform currentSeat;
 
     private void Awake() {
         agent = GetComponent<NavMeshAgent>(); // navmesh is a must for npc to make it walk
@@ -20,5 +25,80 @@ public class CustomerAI : MonoBehaviour
     private void Start() {
         IsCheater = Random.value < cheatChance; // if random value is lower than prob, that npc is cheater
         CurrentState = CustomerState.Idle; // start state of npc (will change due its behaviour)
+
+        GoToBlackjackTable();
     }
+
+    private void Update()
+    {
+        switch (CurrentState)
+        {
+            case CustomerState.WalkingToTable:
+            UpdateWalkingToTable();
+            break;
+
+            case CustomerState.Leaving:
+            UpdateLeaving();
+            break;
+        }
+    }
+
+    private void GoToBlackjackTable() // finds an empty blackjack seat
+    {
+        if (CustomerDestinationManager.Instance.TryGetBlackjackSeat(this, out currentTable, out currentSeat)) // is there any empty seat?
+        {
+            CurrentState = CustomerState.WalkingToTable; //if yes walks to there
+            agent.SetDestination(currentSeat.position);
+        }
+        else
+        {
+            LeaveCasino(); // if no leaves casino
+        }
+    }
+
+    private void UpdateWalkingToTable() // is npc reached to point?
+    {
+        if (HasReachedDestination())
+        {
+            CurrentState = CustomerState.PlayingBlackjack; // if yes npc stops and stars playing bj
+            agent.ResetPath();
+        }
+    }
+
+    private void LeaveCasino() // leave casino
+    {
+        if (currentTable != null)
+        {
+            currentTable.ReleaseSeat(this); // occupied seat reserved for another NPC
+            currentTable = null;
+            currentSeat = null;
+        }
+
+        CurrentState = CustomerState.Leaving; // npc starts walking to exit
+
+        if (CustomerDestinationManager.Instance.ExitPoint != null)
+        {
+            agent.SetDestination(CustomerDestinationManager.Instance.ExitPoint.position);
+        }
+    }
+
+    private void UpdateLeaving() // checks that is npc reached exit
+      {
+          if (HasReachedDestination()) // if yes npc deleted from scene
+          {
+              CurrentState = CustomerState.Gone;
+              Destroy(gameObject);
+          }
+      }
+
+      private bool HasReachedDestination() // if nacmesh still calculating distance the npc not yet left
+      {
+          if (agent.pathPending)
+          {
+              return false;
+          }
+
+          return agent.remainingDistance <= destinationReachedDistance; // returns true if npc gets closer to the exit
+      }
+  
 }
