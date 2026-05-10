9fb2fc97c6da3aa236337a92fb692d44eeb34d14
keremozt <keremoztopuzzz@gmail.com>
2026-05-10 14:59:21 +0300

    feat: add npc enum states

---
+++ Assets/_Scripts/AI/CustomerState.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+// NPC states 
+public enum CustomerState 
+{
+    Idle,
+    WalkingToTable,
+    PlayingBlackjack,
+    Cheating,
+    Suspicious,
+    Interrogating,
+    EscortedToBackRoom,
+    Leaving,
+    Gone
+}
+
