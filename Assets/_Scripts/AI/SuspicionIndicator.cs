fc70c131dc4e12d9bdc45146aac5a480e150287e
keremozt <keremoztopuzzz@gmail.com>
2026-05-11 02:30:14 +0300

    fix: orient suspicion marker toward camera

--- Assets/_Scripts/AI/SuspicionIndicator.cs
+++ Assets/_Scripts/AI/SuspicionIndicator.cs
@@ -2,18 +2,35 @@ using UnityEngine;
 
 public class SuspicionIndicator : MonoBehaviour
 {
-    private void Awake()
+    private void LateUpdate()
     {
-        Hide();
+        if (Camera.main == null)
+        {
+            return;
+        }
+
+        transform.rotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position);
     }
 
     public void Show()
     {
         gameObject.SetActive(true);
+        SetRenderersEnabled(true);
     }
 
     public void Hide()
     {
+        SetRenderersEnabled(false);
         gameObject.SetActive(false);
+    }
+
+    private void SetRenderersEnabled(bool isEnabled)
+    {
+        Renderer[] renderers = GetComponentsInChildren<Renderer>(true);
+
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].enabled = isEnabled;
+        }
     }
 }
